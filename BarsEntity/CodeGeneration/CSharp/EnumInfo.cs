8d14fdd6488388d2c6eaf2680bc41ba59205c1c0
Getequ <getequ@yandex.ru>
2015-08-29 21:51:54 +0600

    v1.4.1 - генерация Enum-типа поля сущности
    Генерация Enum-типа поля сущности, если enum не найден в решении

---
+++ BarsEntity/CodeGeneration/CSharp/EnumInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barsix.BarsEntity.CodeGeneration.CSharp
+{
+    public class EnumInfo : CodeFragment
+    {
+        public string BaseType;
+
+        public List<EnumValue> Values = new List<EnumValue>();
+
+        public override List<string> Generate(int indent)
+        {
+            var list = new List<string>();
+            list.Add(((!string.IsNullOrEmpty(Access) ? Access + " " : "") + "enum " + Name + (!string.IsNullOrEmpty(BaseType) ? " : " + BaseType : "")).Ind(indent));
+            list.Add("{".Ind(indent));
+
+            foreach (var value in Values)
+            {
+                list.Add((value.Name + (value.Value.HasValue ? " = " + value.Value.ToString() : "")).Ind(indent+1));
+            }
+
+            list.Add("}".Ind(indent));
+
+            return list;
+        }
+    }
+
+    public struct EnumValue
+    {
+        public string Name;
+        public long? Value;
+    }
+}
